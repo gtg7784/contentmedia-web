ad05f19c8b280f85a389cc0836b11585b9719d17
gtg7784 <gtg7784@naver.com>
2020-03-28 00:41:16 +0900

    UPDATE: project bolierplate

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
+import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Home from 'containers/Home';
+import Notfound from 'containers/Notfound';
+
 const Container = styled.div`
-  width: 100%;
+  width: 1440px;
   height: 100%;
   background: #f4f8fc;
 `;
@@ -12,7 +16,10 @@ class App extends React.Component {
   render(){
     return (
       <Container>
-
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route exact component={Notfound} />
+        </Switch>
       </Container>
     );
   }
