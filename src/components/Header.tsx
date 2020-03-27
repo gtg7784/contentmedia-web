3ecda2d7e3ffd5b9384950edaf33eda1bff22989
gtg7784 <gtg7784@naver.com>
2020-03-28 00:41:34 +0900

    ADD: Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import logo from 'assets/logo.svg';
+
 // import logo from ''
+
+const Container = styled.header`
+  width: calc(100% - 400px);
+  height: 150px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 200px;
+`;
+const Logo = styled.img`
+  width: 70px;
+  height: 40px;
+`;
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+`;
+
+const Listitem = styled.li`
+  float: left;
+  margin-left: 64px;
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 1.5px;
+  color: #888888;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
 
 interface Props { 
   background: string;
 }
 
-const Container = styled.header`
-  width: 100%;
-  height: 150px;
-`;
-const Logo = styled.img`
-
-`;
-
 const Header: React.FC<Props> = (props: Props) => {
   return (
     <Container style={{ background: props.background}}>
-      <Logo/>
+      <Logo src={logo} />
+      <List>
+        <Listitem>HOME</Listitem>
+        <Listitem>MEMBERS</Listitem>
+        <Listitem>Q&A</Listitem>
+      </List>
     </Container>
   )
 }
