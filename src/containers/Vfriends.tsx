8745d3ea823e95817d148e4a8008e18d6d25bfb2
gtg7784 <gtg7784@naver.com>
2020-04-02 19:36:35 +0900

    ADD: Vfriends Container

---
+++ src/containers/Vfriends.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props { };
+interface State { };
+
+class Vfriends extends React.Component<Props, State>{
+  render(){
+    return(
+      <div/>
+    )
+  }
+}
+
+export default Vfriends;
