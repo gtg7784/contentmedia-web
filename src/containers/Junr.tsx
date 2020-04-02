d343ea07a49f76becbf84593c2b726e5081e899b
gtg7784 <gtg7784@naver.com>
2020-04-02 19:35:55 +0900

    ADD: Junr Container

---
+++ src/containers/Junr.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props { };
+interface State { };
+
+class Junr extends React.Component<Props, State>{
+  render(){
+    return(
+      <div/>
+    )
+  }
+}
+
+export default Junr;
