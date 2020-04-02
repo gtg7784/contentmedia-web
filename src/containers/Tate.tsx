9ea6407508e179f87b4e6cda16b80bf17104c99a
gtg7784 <gtg7784@naver.com>
2020-04-02 19:36:19 +0900

    ADD: Tate Container

---
+++ src/containers/Tate.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props { };
+interface State { };
+
+class Tate extends React.Component<Props, State>{
+  render(){
+    return(
+      <div/>
+    )
+  }
+}
+
+export default Tate;
