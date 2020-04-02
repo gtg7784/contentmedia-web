b98be9dced4d196bcab40ae7be6eb0c31e4743d3
gtg7784 <gtg7784@naver.com>
2020-04-02 19:36:08 +0900

    ADD: Mit Container

---
+++ src/containers/Mir.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props { };
+interface State { };
+
+class Mir extends React.Component<Props, State>{
+  render(){
+    return(
+      <div/>
+    )
+  }
+}
+
+export default Mir;
