20d5fce827912daf615e9f8f642fa20d51eb1f19
gtg7784 <gtg7784@naver.com>
2020-04-02 19:35:43 +0900

    ADD: Aun Container

---
+++ src/containers/Aun.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props { };
+interface State { };
+
+class Aun extends React.Component<Props, State>{
+  render(){
+    return(
+      <div/>
+    )
+  }
+}
+
+export default Aun;
