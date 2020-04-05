4f60b9b26e1720be64c04d7cc2c35bbc917360e2
gtg7784 <gtg7784@naver.com>
2020-04-06 03:02:30 +0900

    UPDATE: Home Container

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -26,7 +26,8 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 1440px;
+  min-width: 1440px;
+  width: 100%;
   height: 100%;
   background: #f4f8fc;
   overflow-x: hidden;
