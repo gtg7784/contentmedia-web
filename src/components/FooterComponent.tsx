d099f6820cf6013d9355557a7d9f472ad68b545a
gtg7784 <gtg7784@naver.com>
2020-04-10 18:38:20 +0900

    UPDATE: footer component

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,19 +1,100 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-import sunrinIcon from 'assets/sunrinIcon.svg';
+import sunrinIcon from 'assets/footer.svg';
+import youtube from 'assets/youtube.svg';
+import facebook from 'assets/facebook.svg';
+import sunrin from 'assets/sunrinicon.svg';
 
 const Container = styled.footer`
   width: 100%;
-  height: 200px;
+  padding-top: 50px;
+  height: 150px;
   background-color: #f0f0f0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+`;
+const ItemWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const Logo = styled.img`
+  margin-bottom: 14px;
+`;
+const Text = styled.p`
+  margin: 0px;
+  font-size: 10px;
+  font-weight: 500;
+  letter-spacing: 0.2px;
+  color: #3f3d56;
+  line-height: 1.6;
+`;
+const BoldText = styled.span`
+  font-size: 14px;
+  font-weight: 800;
+  letter-spacing: 0.2px;
+`;
+const IconWrap = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+`;
+const Icon = styled.a`
+  width: 42px;
+  height: 42px;
+  background-color: #fff;
+  border-radius: 21px;
+  display: flex;
+  flex-direction: row;
+  align-item: center;
+  justify-content: center;
 `;
 interface Props { }
 
 const FooterComponent: React.FC<Props> = () => {
   return(
     <Container>
-      <img src={sunrinIcon} alt=""/>
+      <ItemWrap style={{ marginRight: 164 }}>
+        <Logo src={sunrinIcon} alt=""/>
+        <Text>
+          © 2020 선린인터넷고등학교 멀티미디어과 <br/>
+          All Rights Reserved.<br/>
+          Designed by MINKIM<br/>
+          Developed by TaeGeon Go<br/>
+        </Text>
+      </ItemWrap>
+      <ItemWrap style={{ marginRight: 60 }}>
+        <BoldText>선린인터넷고등학교 콘텐츠디자인과</BoldText>
+        <Text>
+          서울특별시 용산구 원효로97길 33-4<br/>
+          선린인터넷고등학교<br/>
+          3호관 2층 콘텐츠디자인과<br/>
+          Tel. 713-6213  |  Fax. 704-0960<br/>
+        </Text>
+      </ItemWrap>
+      <ItemWrap style={{ marginRight: 98 }}>
+        <BoldText>시연회 제작</BoldText>
+        <Text>
+          강민지 김나현 김민 <br/>
+          고태건 남민우 손성민 <br/>
+          장채림 정한비 최가은 <br/>
+          최서연 최정규 <br/>
+        </Text>
+      </ItemWrap>
+      <IconWrap>
+        <Icon style={{ marginRight: 60 }} href="https://www.youtube.com/channel/UCTuj8x2de_3v6lxxFw_whpw">
+          <img src={youtube} alt=""/>
+        </Icon>
+        <Icon style={{ marginRight: 60 }} href="https://www.facebook.com/선린인터넷고등학교-콘텐츠디자인과-시연회-106949140954012/">
+          <img src={facebook} alt=""/>
+        </Icon>
+        <Icon href="http://sunrint.hs.kr/index.do">
+          <img src={sunrin} alt=""/>
+        </Icon>
+      </IconWrap>
     </Container>
   )
 }
