a19c5e065c13c1fefa29f2f25c05f8d67e8573a3
gtg7784 <gtg7784@naver.com>
2020-04-13 17:38:30 +0900

    UPDATE: provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'globalStyles';
 import App from 'containers/App';
 import * as serviceWorker from './serviceWorker';
 
