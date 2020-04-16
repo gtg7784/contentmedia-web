3460fa870a5e06e506339afa814c5847f814798c
gtg7784 <gtg7784@naver.com>
2020-04-17 00:52:11 +0900

    UPDATE: Vfirends Data

--- src/data/vfriendsData.ts
+++ src/data/vfriendsData.ts
@@ -44,6 +44,79 @@ import artwork43 from 'assets/vfirends/artwork43.png';
 import artwork44 from 'assets/vfirends/artwork44.png';
 import artwork45 from 'assets/vfirends/artwork45.png';
 
+import artwork1_1 from 'assets/vfirends/data/1/1.png';
+import artwork2_1 from 'assets/vfirends/data/2/1.png';
+import artwork2_2 from 'assets/vfirends/data/2/2.png';
+import artwork2_3 from 'assets/vfirends/data/2/3.png';
+import artwork2_4 from 'assets/vfirends/data/2/4.png';
+import artwork2_5 from 'assets/vfirends/data/2/5.png';
+import artwork2_6 from 'assets/vfirends/data/2/6.png';
+import artwork3_1 from 'assets/vfirends/data/3/1.png';
+import artwork3_2 from 'assets/vfirends/data/3/2.png';
+import artwork3_3 from 'assets/vfirends/data/3/3.png';
+import artwork3_4 from 'assets/vfirends/data/3/4.png';
+import artwork3_5 from 'assets/vfirends/data/3/5.png';
+import artwork3_6 from 'assets/vfirends/data/3/6.png';
+import artwork3_7 from 'assets/vfirends/data/3/7.png';
+import artwork3_8 from 'assets/vfirends/data/3/8.png';
+import artwork3_9 from 'assets/vfirends/data/3/9.png';
+import artwork3_10 from 'assets/vfirends/data/3/10.png';
+import artwork3_11 from 'assets/vfirends/data/3/11.png';
+import artwork3_12 from 'assets/vfirends/data/3/12.png';
+import artwork3_13 from 'assets/vfirends/data/3/13.png';
+import artwork3_14 from 'assets/vfirends/data/3/14.png';
+import artwork3_15 from 'assets/vfirends/data/3/15.png';
+import artwork3_16 from 'assets/vfirends/data/3/16.png';
+import artwork3_17 from 'assets/vfirends/data/3/17.png';
+import artwork3_18 from 'assets/vfirends/data/3/18.png';
+import artwork4_1 from 'assets/vfirends/data/4/1.png';
+import artwork4_2 from 'assets/vfirends/data/4/2.png';
+import artwork4_3 from 'assets/vfirends/data/4/3.jpg';
+import artwork5_1 from 'assets/vfirends/data/5/1.jpg';
+import artwork6_1 from 'assets/vfirends/data/6/1.png';
+import artwork6_2 from 'assets/vfirends/data/6/2.png';
+import artwork6_3 from 'assets/vfirends/data/6/3.png';
+import artwork6_4 from 'assets/vfirends/data/6/4.png';
+import artwork6_5 from 'assets/vfirends/data/6/5.png';
+import artwork6_6 from 'assets/vfirends/data/6/6.png';
+import artwork7_1 from 'assets/vfirends/data/7/1.png';
+import artwork14_1 from 'assets/vfirends/data/14/1.jpg';
+import artwork15_1 from 'assets/vfirends/data/15/1.jpg';
+import artwork16_1 from 'assets/vfirends/data/16/1.jpg';
+import artwork17_1 from 'assets/vfirends/data/17/1.jpg';
+import artwork18_1 from 'assets/vfirends/data/18/1.jpg';
+import artwork20_1 from 'assets/vfirends/data/20/1.jpg';
+import artwork21_1 from 'assets/vfirends/data/21/1.jpg';
+import artwork21_2 from 'assets/vfirends/data/21/2.jpg';
+import artwork22_1 from 'assets/vfirends/data/22/1.png';
+import artwork23_1 from 'assets/vfirends/data/23/1.jpg';
+import artwork24_1 from 'assets/vfirends/data/24/1.jpg';
+import artwork25_1 from 'assets/vfirends/data/25/1.png';
+import artwork26_1 from 'assets/vfirends/data/26/1.jpg';
+import artwork27_1 from 'assets/vfirends/data/27/1.jpg';
+import artwork28_1 from 'assets/vfirends/data/28/1.jpg';
+import artwork29_1 from 'assets/vfirends/data/29/1.jpg';
+import artwork30_1 from 'assets/vfirends/data/30/1.jpg';
+import artwork30_2 from 'assets/vfirends/data/30/2.jpg';
+import artwork30_3 from 'assets/vfirends/data/30/3.jpg';
+import artwork30_4 from 'assets/vfirends/data/30/4.jpg';
+import artwork31_1 from 'assets/vfirends/data/31/1.png';
+import artwork32_1 from 'assets/vfirends/data/32/1.png';
+import artwork33_1 from 'assets/vfirends/data/33/1.png';
+import artwork34_1 from 'assets/vfirends/data/34/1.jpg';
+import artwork35_1 from 'assets/vfirends/data/35/1.jpg';
+import artwork36_1 from 'assets/vfirends/data/36/1.jpg';
+import artwork37_1 from 'assets/vfirends/data/37/1.png';
+import artwork38_1 from 'assets/vfirends/data/38/1.jpg';
+import artwork39_1 from 'assets/vfirends/data/39/1.jpg';
+import artwork40_1 from 'assets/vfirends/data/40/1.jpg';
+import artwork41_1 from 'assets/vfirends/data/41/1.jpg';
+import artwork42_1 from 'assets/vfirends/data/42/1.jpg';
+import artwork43_1 from 'assets/vfirends/data/43/1.jpg';
+import artwork44_1 from 'assets/vfirends/data/44/1.jpg';
+import artwork45_1 from 'assets/vfirends/data/45/1.jpg';
+import artwork45_2 from 'assets/vfirends/data/45/2.jpg';
+
 const vfriendsData: Array<ArtworkType> = [
   {
     thumbnail: artwork1,
@@ -52,7 +125,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "나에 대한 정보를 인포그래픽으로 나타내었다. 기본적인 정보들과 취미, 관심사, 다룰 수 있는 프로그램이 주 내용이다. 간결한 느낌을 주기…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork1_1],
       profile: {
         name: '곽승채',
         email: 'tmdcoTm03@gmail.com',
@@ -66,7 +139,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "UI/UX",
     description: "버스킹 공연을 즐기는 이는 정말 많다. 하지만 버스커 이름을 알고 듣는 사람은 많을까? 왜 밴드 가수들이 재능을 가져도 기억되지 못할까?…",
     data: {
-      images: [],
+      images: [artwork2_1, artwork2_2, artwork2_3, artwork2_4, artwork2_5, artwork2_6],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -81,7 +154,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "LOGO DESIGN",
     description: "외주로 진행했던 정당 로고 제작 프로젝트다. 소통과 공유, 혁신을 강조하는, 작지만 더 큰 정당을 모토로 구상했고 프레젠테이션까지 맡아서…",
     data: {
-      images: [],
+      images: [artwork3_1, artwork3_2, artwork3_3, artwork3_4, artwork3_5, artwork3_6, artwork3_7, artwork3_8, artwork3_9, artwork3_10, artwork3_11, artwork3_12, artwork3_13, artwork3_14, artwork3_15, artwork3_16, artwork3_17, artwork3_18],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -95,7 +168,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "3D",
     description: "몇 십년이 지나도 구조변경이 어려운 지하철 역사는 첫 설계시부터 큰 고민을 해나가며 미래에도 뒤쳐지지 않을 듯한 ‘미래지향적’ 설계를…",
     data: {
-      images: [],
+      images: [artwork4_1, artwork4_2, artwork4_3],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -110,7 +183,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "2014년 7월, 한 중년 남성이 전봇대에 오르다 감전 당한 사건을 기억하는가? 그는 경범죄로 조사받은 것에 항의하려 전봇대에 올라갔다가…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork5_1],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -124,7 +197,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "3D",
     description: "3D 실내 공모전에 출품했던 3D 모델링 작품이다. 수상하지 못해 아쉬웠으나 제작 과정에서 느꼈던 보람과 성취는 상을 받지 못하여도…",
     data: {
-      images: [],
+      images: [artwork6_1, artwork6_2, artwork6_3, artwork6_4, artwork6_5, artwork6_6],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -138,7 +211,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "3D",
     description: "몇 십년이 지나도 구조변경이 어려운 지하철 역사는 첫 설계시부터 큰 고민을 해나가며 미래에도 뒤쳐지지 않을 듯한 ‘미래지향적’ 설계를…",
     data: {
-      images: [],
+      images: [artwork7_1],
       profile: {
         name: '김민',
         email: 'kimmin4154@naver.com',
@@ -185,8 +258,8 @@ const vfriendsData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
+        name: '김민준',
+        email: 'sheepwin@naver.com',
         description: '브이프렌즈의 홍보영상이다. 나레이션을 삽입하였고 모든 일러스트를 직접 제작하였다.<br/><br/>영상에는 브이프렌즈의 수상 내역등의 다양한 정보를 담았다.'
       },
       link: "https://www.youtube.com/watch?v=52l_2UQp6zw"
@@ -244,7 +317,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "PHOTO",
     description: "직접 DSLR 카메라와 망원렌즈를 이용하여 찍고 포토샵으로 보정한 사진이다. 평소 다른 작품들의 밝은 색감과는 달리 랩과 힙합이라는 장르…",
     data: {
-      images: [],
+      images: [artwork14_1],
       profile: {
         name: '김예린',
         email: 'luckyriny@gmail.com',
@@ -258,7 +331,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "좋아하는 가수의 사진을 이용하여 포스터 디자인을 작업했다. 한결 단조로울 수 있는 배경에 포토샵의 블렌드 기능을 활용하여 우주에 있는…",
     data: {
-      images: [],
+      images: [artwork15_1],
       profile: {
         name: '김예린',
         email: 'luckyriny@gmail.com',
@@ -272,7 +345,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "좋아하는 가수의 사진을 내 그림체로 변화시켜 그린 것이다. 자칫 단조로울 수 있는 배경에는 캘리그라피 폰트로 이름을 적어 넣었다. 본 사진…",
     data: {
-      images: [],
+      images: [artwork16_1],
       profile: {
         name: '김예린',
         email: 'luckyriny@gmail.com',
@@ -287,7 +360,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "딱히 뜻은 없고 그냥 누워있다가 그림이 그리고 싶어져서 그렸다. 색은 굳이 칠하지 않았고 흑백이 더욱 어울리는 느낌인 것 같아 흑백으로 …",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork17_1],
       profile: {
         name: '김예린',
         email: 'luckyriny@gmail.com',
@@ -301,7 +374,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "브이프렌즈 단체사진의 색감을 보정하고 직접 쓴 캘리그라피를 삽입해 포스터 디자인을 하였다. 원본 사진이 가로로 잘린 사진이라 위의 하늘…",
     data: {
-      images: [],
+      images: [artwork18_1],
       profile: {
         name: '김예린',
         email: 'luckyriny@gmail.com',
@@ -330,7 +403,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "눈에 띄는 느낌을 주고 싶어서 네온사인으로 만들었다. 대표적으로 주황색, 핑크색, 보라색 위주의 색상을 사용해 열정적이고 활발한 느낌을…",
     data: {
-      images: [],
+      images: [artwork20_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -345,7 +418,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "선린제 부스 홍보 포스터 1과 같은 시리즈로 만들었다. 홍보를 위한 포스터가 아닌 사람들에게 알리기 위한 메뉴판 포스터인만큼 내용을…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork21_1, artwork21_2],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -359,7 +432,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "COLLAGE",
     description: "보통 검은색의 이미지는 부정적이다. 하지만 나는 검은색을 모든 색이 섞인 최종 단계 즉, 내가 도달할 마지막 모습이라고 생각한다. 지금…",
     data: {
-      images: [],
+      images: [artwork22_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -373,7 +446,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "좋아하는 중국 배우를 폴리곤으로 표현하면 어떨까? 해서 만들게 되었다. 빨간색과 핑크색 위주로 사용하여 화려한 사극 느낌을 더했다. …",
     data: {
-      images: [],
+      images: [artwork23_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -387,7 +460,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "COLLAGE",
     description: "유토피아란? 현실에는 존재하지 않는 이상적인 세계, 이상향(理想鄕) 키워드는 사랑, 그네, 달, 봄, 우주, 고래다. 전체적으로 몽환적인 …",
     data: {
-      images: [],
+      images: [artwork24_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -402,7 +475,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "어릴 적, 내 꿈은 마녀였다. 구름을 보며 무엇인가를 떠올리는 것을 즐겼고 마법을 부리기 위해 주문을 외우는 것도 서슴치 않았다. 커 갈수록…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork25_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -416,7 +489,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "INFOGRAPHIC",
     description: "어렸을 때 가장 좋아하는 만화가 호빵맨이였다. 그때의 분위기를 회상하고 호빵맨에 대해 잘 모르는 사람들에게 소개하기 위해 제작했다….",
     data: {
-      images: [],
+      images: [artwork26_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -430,7 +503,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "CHARACTER DESIGN",
     description: "평소에 동물이나 사물의 이름을 사용해 말장난을 하는 것을 좋아했다. 직접 캐릭터를 만들고 관련된 동물 잠옷을 입히면 어떨까? 해서 제작…",
     data: {
-      images: [],
+      images: [artwork27_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -444,7 +517,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "한참 다시 떠오르고 있는 문제였다. 찾아보니 내가 모르는 정말 다양한 종류의 마약이 있었고 많은 사람들에게 경각심을 주고자 마약을 주제로…",
     data: {
-      images: [],
+      images: [artwork28_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -459,7 +532,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "항상 다른 사람들의 인포그래픽을 봐오다 나를 나타내는 인포그래픽을 만들고자해서 모작으로 제작하게 되었다. 가장 좋아하는 색이자 나를…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork29_1],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -473,7 +546,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "체육대회 대진표를 만들었다. 글자만 넣어 밋밋하지 않게 하단은 아이소매트릭으로 관련된 운동과 같게 제작했다. 사람을 넣어 생동감 있고 …",
     data: {
-      images: [],
+      images: [artwork30_1, artwork30_2, artwork30_3, artwork30_4],
       profile: {
         name: '성지빈',
         email: 'sgb0624@naver.com',
@@ -487,7 +560,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "CHARACTER DESIGN",
     description: "대한민국은 굉장히 발달한 선진국이지만, 선진국 같지 않은 면모를 보일 때가 많다. OECD 국가 중 자살률, 노인빈곤율, 노동시간 등 여러가지…",
     data: {
-      images: [],
+      images: [artwork31_1],
       profile: {
         name: '정준혁',
         email: 'wwj8481@naver.com',
@@ -501,7 +574,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "내가 마주한 인생을 표현하였다. 정면에는 장애물이 다가오고 주변 분위기는 어둡지만 이를 견디고 나아가야 한다.",
     data: {
-      images: [],
+      images: [artwork32_1],
       profile: {
         name: '정준혁',
         email: 'wwj8481@naver.com',
@@ -516,7 +589,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "선린 SSF 나눔축제 브이프렌즈 교재를 여러 부원이 함꼐 제작했다. ",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork33_1],
       profile: {
         name: '브이프렌즈',
         email: '',
@@ -530,7 +603,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "POSTER",
     description: "우리 사회가 학생들에게 무조건 공부를 하라는 것에 대해 의문을 품고 만든 작품이다.왜 우리의 인생은 첫번째 선택지가 모두 ctrl c …",
     data: {
-      images: [],
+      images: [artwork34_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -544,7 +617,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "사람에다가 핑크를 끼얹었다. 모든 색을 핑크로 도배했다. 분홍색의 부드러움을 표현하기 위해 선을 따지 않고 명암을 주었으며, 펜 굵기를 …",
     data: {
-      images: [],
+      images: [artwork35_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -558,7 +631,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "ILLSTRATION",
     description: "외로운 바다 한 가운데 동떨어진 사람을 그렸다. 외롭고 쓸쓸한 느낌을 강조하기 위해 흑백으로 표현했다. 영혼이 없어 보이는 표정과 축처진…",
     data: {
-      images: [],
+      images: [artwork36_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -573,7 +646,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "난색 계열만을 사용해 인물을 표현했다. 배경에도 노을이 진 하늘을 흐리게 합성했다. 따뜻한 색을 사용했지만, 인물은 어딘가 쓸쓸해보인다…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork37_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -587,7 +660,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "INFOGRAPHIC",
     description: "'메세지가 미디어다'라는 책을 읽고 '모바일 부족화'의 개념에 대해서 알게 되었다. 새로운 사회에 새롭게 등장하는 그들에 대해 많은 사람들…",
     data: {
-      images: [],
+      images: [artwork38_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -601,7 +674,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "CHARACTER DESIGN",
     description: "뷰티 유튜버 '리수'님의 주작 메이크업을 보고 캐릭터 디자인을 했다.  화난 얼굴상에 얇은 입, 진한 붉은 메이크업으로 새를 의인화했다. …",
     data: {
-      images: [],
+      images: [artwork39_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -615,7 +688,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "EDITORIAL DESIGN",
     description: "호텔 델루나의 달이 객잔을 주제로 타이틀 디자인을 했다. 두가지 색깔을 사용해서 차분하고 담담한 인상을 표현했다. 한자, 얇은 세리프체, …",
     data: {
-      images: [],
+      images: [artwork40_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -630,7 +703,7 @@ const vfriendsData: Array<ArtworkType> = [
     description: "'피터팬'에 나오는 네버렌드는 환상 속의 나라이다. 현실에서 그 환상 너머로 데려다주는 특별한 말을 표현해봤다. 알록달록한 다양한 색채를…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork41_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -644,7 +717,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "EDITORIAL DESIGN",
     description: "'메세지가 미디어다'라는 책을 읽고 '모바일 부족화'의 개념에 대해서 알게 되었다. 새로운 사회에 새롭게 등장하는 그들에 대해 많은 사람들…",
     data: {
-      images: [],
+      images: [artwork42_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -658,7 +731,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "EDITORIAL DESIGN",
     description: "'그 시절, 우리_나의 이야기가 담긴 디자인'에 대해서 발표할 때 만든 PPT 표지이다. 나는 빨강머리 앤과 모래요정 바람돌이의 애니메이션…",
     data: {
-      images: [],
+      images: [artwork43_1],
       profile: {
         name: '최서연',
         email: 'sally1357246@gmail.com',
@@ -672,7 +745,7 @@ const vfriendsData: Array<ArtworkType> = [
     type: "EDITORIAL DESIGN",
     description: '타이틀 디자인: 떠나자 해외여행 네팔 국기색인 노란색과 빨간색을 사용하였다. "떠나자"라는 문구에 맞게 신나고 활발한 …',
     data: {
-      images: [],
+      images: [artwork44_1],
       profile: {
         name: '최원호',
         email: 'zumu52@naver.com',
@@ -687,10 +760,10 @@ const vfriendsData: Array<ArtworkType> = [
     description: "'피터팬'에 나오는 네버렌드는 환상 속의 나라이다. 현실에서 그 환상 너머로 데려다주는 특별한 말을 표현해봤다. 알록달록한 다양한 색채를…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork45_1, artwork45_2],
       profile: {
-        name: '',
-        email: '',
+        name: '김예린',
+        email: 'luckyriny@gmail.com',
         description: ''
       },
     }
