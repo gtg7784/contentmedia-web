cfaa5c4b58f86b2102fdea6170bc24efe288335e
gtg7784 <gtg7784@naver.com>
2020-04-16 16:52:34 +0900

    UPDATE: data

---
+++ src/data/index.ts
@@ -0,0 +1,13 @@
+import aunData from './aunData';
+import junrData from './junrData';
+import mirData from './mirData';
+import tateData from './tateData';
+import vfriendsData from './vfriendsData';
+
+export default {
+  AunData: aunData,
+  JunrData: junrData,
+  MirData: mirData,
+  TateData: tateData,
+  VfriendsData: vfriendsData
+}
