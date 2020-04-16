a8bb6d28995d73462013bd63790543d74c145b9c
gtg7784 <gtg7784@naver.com>
2020-04-16 09:07:21 +0900

    UPDATE: Tate data

--- src/containers/Tate.tsx
+++ src/containers/Tate.tsx
@@ -105,6 +105,7 @@ interface Artwork {
   type: string;
   description: string;
   style?: Object;
+  data: DetailType;
 }
 interface Props { };
 interface State {
@@ -122,219 +123,499 @@ class Tate extends React.Component<Props, State>{
           title: "할로윈 콜라주_김나현",
           type: "COLLAGE",
           description: "테이트 축제 굿즈인 스티커로 제작했던 할로윈 콜라주이다.",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '테이트 축제 굿즈인 스티커로 제작했던 할로윈 콜라주이다.'
+            },
+          }
         },
         {
           img: artwork2,
           title: "꿈같은 유토피아 콜라주 ver.1",
           type: "COLLAGE",
           description: "내가 바라는 유토피아를 표현한 콜라주이다. 유토피아라는 이름답게 내가 좋아하는 색 계열로 구성했다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '내가 바라는 유토피아를 표현한 콜라주이다. 유토피아라는 이름답게 내가 좋아하는 색 계열로 구성했다.'
+            },
+          }
         },
         {
           img: artwork3,
           title: "꿈같은 유토피아 콜라주 ver.2",
           type: "COLLAGE",
           description: "내가 바라는 유토피아를 표현한 콜라주이다. 유토피아라는 이름답게 내가 좋아하는 색 계열로 구성했다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '내가 바라는 유토피아를 표현한 콜라주이다. 유토피아라는 이름답게 내가 좋아하는 색 계열로 구성했다.'
+            },
+          }
         },
         {
           img: artwork4,
           title: "colors_김나현",
           type: "ALBUM COVER",
           description: "데이식스의 ‘colors’를 들으며 작업한 앨범 커버 아트이다. 노래의 분위기와 가사를 모두 반영해 열심히 작업했다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '데이식스의 ‘colors’를 들으며 작업한 앨범 커버 아트이다. 노래의 분위기와 가사를 모두 반영해 열심히 작업했다.'
+            },
+          }
         },
         {
           img: artwork5,
           title: "선린축제 포스터_김나현",
           type: "POSTER",
           description: "2019 선린축제의 포스터를 맡아서 작업했다. 다양한 사람들이 하나가 되어 모이는 것을 표현하고자 각각 색감이 상반되는 세가지의 색을…",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '2019 선린축제의 포스터를 맡아서 작업했다. 다양한 사람들이 하나가 되어 모이는 것을 표현하고자 각각 색감이 상반되는 세가지의 색을 주로 사용하여 작업했다.'
+            },
+          }
         },
         {
           img: artwork6,
           title: "2020 신년엽서_김나현",
           type: "EDITORIAL DESIGN",
           description: "반 친구들과 함께 2020년을 맞이하기 위해 만든 신년엽서이다. 2020년 컬러인 클래식 블루를 주로 사용하였고 각각의 선택에 따라 각자 그에…",
+          data: {
+            images: [],
+            profile: {
+              name: '김나현',
+              email: '',
+              description: '반 친구들과 함께 2020년을 맞이하기 위해 만든 신년엽서이다. 2020년 컬러인 클래식 블루를 주로 사용하였고 각각의 선택에 따라 각자 그에 맞는 다른 해가 뜰 테니 어떤 선택이건 자신을 가지고 2020년을 보내라는 의미를 담고있다.'
+            },
+          }
         },
         {
           img: artwork7,
           title: "이스케이프_김상현",
           type: "ALBUM COVER",
           description: "외주를 받은 작품인데 음악 이름이 ‘이스케이프’라고 해서 그에 맞는 분위기를 조성했습니다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김상현',
+              email: '',
+              description: '외주를 받은 작품인데 음악 이름이 ‘이스케이프’라고 해서 그에 맞는 분위기를 조성했습니다.'
+            },
+          }
         },
         {
           img: artwork8,
           title: "moon_김상현",
           type: "COLLAGE",
           description: "인스타를 둘러보던 중 어떤 분의 콜라주 작품을 보고 영감을 받아 제작하게 됐습니다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김상현',
+              email: '',
+              description: '인스타를 둘러보던 중 어떤 분의 콜라주 작품을 보고 영감을 받아 제작하게 됐습니다.'
+            },
+          }
         },
         {
           img: artwork9,
           title: "HAPPY HALLOWEEN",
           type: "ILLUSTRATION",
           description: "학교축제 테이트 부서에서 스티커로 내려고 만든 작품입니다. 도트로 최대한 할로윈 분위기를 살려 만든 작품입니다.",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '김상현',
+              email: '',
+              description: '학교축제 테이트 부서에서 스티커로 내려고 만든 작품입니다. 도트로 최대한 할로윈 분위기를 살려 만든 작품입니다.'
+            },
+          }
         },
         {
           img: artwork10,
           title: "LOVEY_김상현",
           type: "CHARACTER DESIGN",
           description: "캐릭터 디자인 수행평가를 위해 만든 작품입니다.",
+          data: {
+            images: [],
+            profile: {
+              name: '김상현',
+              email: '',
+              description: '캐릭터 디자인 수행평가를 위해 만든 작품입니다.'
+            },
+          }
         },
         {
           img: artwork11,
           title: "Re_김상현",
           type: "INFOGRAPHIC",
           description: "제로부터 시작하는 이 세계에 대해, 인포그래픽 수행평가를 위해 만든 타이틀입니다",
+          data: {
+            images: [],
+            profile: {
+              name: '김상현',
+              email: '',
+              description: '제로부터 시작하는 이 세계에 대해, 인포그래픽 수행평가를 위해 만든 타이틀입니다.'
+            },
+          }
         },
         {
           img: artwork12,
           title: "Trudy_박은별",
           type: "ALBUM COVER",
           description: "",
+          data: {
+            images: [],
+            profile: {
+              name: '박은별',
+              email: '',
+              description: ''
+            },
+          }
         },
         {
           img: artwork13,
           title: "Neverland_박은별",
           type: "ILLUSTRATION",
           description: "",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '박은별',
+              email: '',
+              description: ''
+            },
+          }
         },
         {
           img: artwork14,
           title: "Charmant_박은별",
           type: "EDITORIAL DESIGN",
           description: "‘Charmant’는 매력적인 이라는 뜻을 가진 불어로 패션 관련 포스터나 잡지에 맞는 컨셉으로 디자인했다.  평범함에서 벗어나 이미지와 …",
+          data: {
+            images: [],
+            profile: {
+              name: '박은별',
+              email: '',
+              description: "콜라주 느낌으로 만든 디자인이다.<br/>‘Charmant’는 매력적인 이라는 뜻을 가진 불어로 패션 관련 포스터나 잡지에 맞는 컨셉으로 디자인했다. 평범함에서 벗어나 이미지와 도형을 자유롭게 사용하여 독특한 분위기를 냈다"
+            },
+          }
         },
         {
           img: artwork15,
           title: "JR(day)_박은별",
           type: "COLLAGE",
           description: "콜라주 느낌으로 만든 디자인이다. 배경을 흰색으로 정하여 뜻한 청소년의 순수함 위에 여러 색과 기하학적인 도형과 선들을 이용해 호기심…",
+          data: {
+            images: [],
+            profile: {
+              name: '박은별',
+              email: '',
+              description: '이건 약간 나의 하루 또는 나의 성격을 나타내고자 한 작품이다. <br/>배경을 흰색으로 정하여 뜻한 청소년의 순수함 위에 여러 색과 기하학적인 도형과 선들을 이용해 호기심 많고 발랄한 모습을 담았다. <br/>깔끔하고 명확하게 나타내고자 한 게 목적이었는데 무작정 시작하고 보니 더 잘할 수 있었던 것 같다.'
+            },
+          }
         },
         {
           img: artwork16,
           title: "청소년 장기프로젝트_송지연",
           type: "POSTER",
           description: "제가 활동하고 있는 단체의 포스터 디자인입니다. 단체의 팀컬러인 주황색으로 단체의 열정을 형상화시켰습니다.",
+          data: {
+            images: [],
+            profile: {
+              name: '송지연',
+              email: '',
+              description: '제가 활동하고 있는 단체의 포스터 디자인입니다. 단체의 팀컬러인 주황색으로 단체의 열정을 형상화시켰습니다.'
+            },
+          }
         },
         {
           img: artwork17,
           title: "strawberry cake_송지연",
           type: "ILLUSTRATION",
           description: "신비로운 느낌을 주기 위해 보라 계열을 사용하여 작업한 딸기 케이크 픽셀아트입니다. ",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '송지연',
+              email: '',
+              description: '신비로운 느낌을 주기 위해 보라 계열을 사용하여 작업한 딸기 케이크 픽셀아트입니다.'
+            },
+          }
         },
         {
           img: artwork18,
           title: "strawberry tarte_송지연",
           type: "ILLUSTRATION",
           description: "'strawberry cake'와 함께 작업한 픽셀아트로, 진한 핑크색을 사용하여 앞서 작업한 픽셀아트에 비해 귀여운 느낌을 나타냈습니다….",
+          data: {
+            images: [],
+            profile: {
+              name: '송지연',
+              email: '',
+              description: "'strawberry cake'와 함께 작업한 픽셀아트로, 진한 핑크색을 사용하여 앞서 작업한 픽셀아트에 비해 귀여운 느낌을 나타냈습니다."
+            },
+          }
         },
         {
           img: artwork19,
           title: "merry christmas_송지연",
           type: "EDITORIAL DESIGN",
-          description: "크리스마스의 분위기를 살린 그래픽디자인입니다. 레트로한 느낌을 나타내기 위해 빛바랜 느낌의 색상을 이용하여 작업하였습니다….",
+          description: "크리스마스의 분위기를 살린 그래픽디자인입니다. 레트로한 느낌을 나타내기 위해 빛바랜 느낌의 색상을 이용하여 작업하였습니다",
+          data: {
+            images: [],
+            profile: {
+              name: '송지연',
+              email: '',
+              description: '크리스마스의 분위기를 살린 그래픽디자인입니다. 레트로한 느낌을 나타내기 위해 빛바랜 느낌의 색상을 이용하여 작업하였습니다.'
+            },
+          }
         },
         {
           img: artwork20,
           title: "페퍼톤스 인포그래픽_송지연",
           type: "INFOGRAPHIC",
           description: "가수 '페퍼톤스'의 기본적인 정보가 담겨있는 그래픽디자인입니다.",
+          data: {
+            images: [],
+            profile: {
+              name: '송지연',
+              email: '',
+              description: "가수 '페퍼톤스'의 기본적인 정보가 담겨있는 그래픽디자인입니다."
+            },
+          }
         },
         {
           img: artwork21,
           title: "반딧불이_유혜진",
           type: "ILLUSTRATION",
           description: "할로윈을 주제로 한 일러스트",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '유혜진',
+              email: '',
+              description: '할로윈을 주제로 한 일러스트'
+            },
+          }
         },
         {
           img: artwork22,
           title: "sparkle_유혜진",
           type: "ILLUSTRATION",
           description: "'strawberry cake'와 함께 작업한 픽셀아트로, 진한 핑크색을 사용하여 앞서 작업한 픽셀아트에 비해 귀여운 느낌을 나타냈습니다….",
+          data: {
+            images: [],
+            profile: {
+              name: '유혜진',
+              email: '',
+              description: ''
+            },
+          }
         },
         {
           img: artwork23,
           title: "hot girl bummer_유혜진",
           type: "ILLUSTRATION",
           description: "blackbear의 노래 hot girl bummer의 자켓사진을 재해석한 일러스트",
+          data: {
+            images: [],
+            profile: {
+              name: '유혜진',
+              email: '',
+              description: 'blackbear의 노래 hot girl bummer의 자켓사진을 재해석한 일러스트'
+            },
+          }
         },
         {
           img: artwork24,
           title: "케이크_정재우",
           type: "ILLUSTRATION",
           description: "오로지 수채화 물감을 이용해 그린 케이크이다.",
+          data: {
+            images: [],
+            profile: {
+              name: '정제우',
+              email: '',
+              description: "오로지 수채화 물감을 이용해 그린 케이크이다.",
+            },
+          }
         },
         {
           img: artwork25,
           title: "지하철_정재우",
           type: "ILLUSTRATION",
-          description: "포스터물감과 수채화 물감을 이용해 그린 지하철이다.	",
-          style: { marginLeft: 0 }
+          description: "포스터물감과 수채화 물감을 이용해 그린 지하철이다.",
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '정제우',
+              email: '',
+              description: "포스터물감과 수채화 물감을 이용해 그린 지하철이다.",
+            },
+          }
         },
         {
           img: artwork26,
           title: "할로윈 달_정재우",
           type: "ILLUSTRATION",
           description: "2019년 테이트 할로윈 파티를 맞이하여 손거울에 썼던 디자인이다. 달이 나무에 기생되고있는 것과 그 안에 있는 묘지와 좀비는 할로윈 …",
+          data: {
+            images: [],
+            profile: {
+              name: '정제우',
+              email: '',
+              description: '2019년 테이트 할로윈 파티를 맞이하여 손거울에 썼던 디자인이다. 달이 나무에 기생되고있는 것과 그 안에 있는 묘지와 좀비는 할로윈 분위기를 조성한다.'
+            },
+          }
         },
         {
           img: artwork27,
           title: "베놈 & 카니지_정재우",
           type: "ILLUSTRATION",
           description: "마블에 나오는 베놈과 그의 숙적 카니지를 대치하는 그림으로써 빨간색 배경으로 정열적이고 격렬한 느낌을 준다.",
+          data: {
+            images: [],
+            profile: {
+              name: '정제우',
+              email: '',
+              description: '마블에 나오는 베놈과 그의 숙적 카니지를 대치하는 그림으로써 빨간색 배경으로 정열적이고 격렬한 느낌을 준다.'
+            },
+          }
         },
         {
           img: artwork28,
           title: "자기소개 인포그래픽_정한비",
           type: "INFOGRAPHIC",
           description: "‘나’를 소개하기 위한 인포그래픽이다. 차분한 색채를 통해 깔끔한 느낌으로 제작하였다. ",
+          data: {
+            images: [],
+            profile: {
+              name: '정한비',
+              email: '',
+              description: "‘나’를 소개하기 위한 인포그래픽이다. 차분한 색채를 통해 깔끔한 느낌으로 제작하였다. "
+            },
+          }
         },
         {
           img: artwork29,
           title: "<까오> 캐릭터디자인_정한비",
           type: "CHARACTER DESIGN",
           description: "평소 좋아하던 가오리를 캐릭터로 디자인하여 이모티콘 형식으로 만들었다. 6종류의 표정과 기본형을 제작하여 디자인한 캐릭터를 소개…",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '정한비',
+              email: '',
+              description: '평소 좋아하던 가오리를 캐릭터로 디자인하여 이모티콘 형식으로 만들었다. 6종류의 표정과 기본형을 제작하여 디자인한 캐릭터를 소개하는 판넬을 제작하였다.'
+            },
+          }
         },
         {
           img: artwork30,
           title: "그럴 수 있구나_정한비",
           type: "ALBUM COVER",
           description: "‘신예찬, 정한결, 김민아의 <그럴 수 있구나>’의 앨범아트이다. 사진을 보정하고 디자인해 제작하였다. ",
+          data: {
+            images: [],
+            profile: {
+              name: '정한비',
+              email: '',
+              description: "‘신예찬, 정한결, 김민아의 <그럴 수 있구나>’의 앨범아트이다. 사진을 보정하고 디자인해 제작하였다. "
+            },
+          }
         },
         {
           img: artwork31,
           title: "라즈베리 비트_정한비",
           type: "EDITORIAL DESIGN",
           description: "실사 촬영한 사진에 노이즈와 배경 보정으로 레트로한 느낌을 추가하여 과거와 현재가 융합된 느낌의 표지를 디자인하였다. 밤이지만…",
+          data: {
+            images: [],
+            profile: {
+              name: '정한비',
+              email: '',
+              description: ''
+            },
+          }
         },
         {
           img: artwork32,
           title: "메이데이메이데이",
           type: "EDITORIAL DESIGN",
           description: "‘로맨틱펀치의 <메이데이 메이데이>의 키네틱 타이포그래피 영상이다. 몽환적이고 빠른 우주 공간을 표현하였고 밴드의 영상을 넣는 등 …",
+          data: {
+            images: [],
+            profile: {
+              name: '정한비',
+              email: '',
+              description: '로맨틱펀치의 <메이데이 메이데이>의 키네틱 타이포그래피 영상이다. 몽환적이고 빠른 우주 공간을 표현하였고 밴드의 영상을 넣는 등 다양한 시도를 하였다.'
+            },
+          }
         },
         {
           img: artwork33,
           title: "해피 스켈레톤_조민준",
           type: "ILLUSTRATION",
           description: "축제 때 동아리 굿즈로 제작되었다.",
-          style: { marginLeft: 0 }
+          style: { marginLeft: 0 },
+          data: {
+            images: [],
+            profile: {
+              name: '조민준',
+              email: '',
+              description: '축제 때 동아리 굿즈로 제작되었다.'
+            },
+          }
         },
         {
           img: artwork34,
           title: "패션 잡지 표지 디자인_조민준",
           type: "EDITORIAL DESIGN",
           description: "실사 촬영한 사진에 노이즈와 배경 보정으로 레트로한 느낌을 추가하여 과거와 현재가 융합된 느낌의 표지를 디자인하였다. 공간의 특징을…",
+          data: {
+            images: [],
+            profile: {
+              name: '조민준',
+              email: '',
+              description: '실사 촬영한 사진에 노이즈와 배경 보정으로 레트로한 느낌을 추가하여 과거와 현재가 융합된 느낌의 표지를 디자인하였다. 공간의 특징을 살려 보정하였다.'
+            },
+          }
         },
         {
           img: artwork35,
           title: "패션 잡지 표지 디자인_조민준",
           type: "EDITORIAL DESIGN",
           description: "실사 촬영한 사진에 노이즈와 배경 보정으로 레트로한 느낌을 추가하여 과거와 현재가 융합된 느낌의 표지를 디자인하였다. 밤이지만…",
+          data: {
+            images: [],
+            profile: {
+              name: '조민준',
+              email: '',
+              description: '실사 촬영한 사진에 노이즈와 배경 보정으로 레트로한 느낌을 추가하여 과거와 현재가 융합된 느낌의 표지를 디자인하였다. 밤이지만 밝은 느낌을 주기 위해 노력했다.'
+            },
+          }
         },
       ]
     }
