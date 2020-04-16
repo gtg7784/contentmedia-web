d880a15c621e0f21fe2bb7d94cfa1dd259459227
gtg7784 <gtg7784@naver.com>
2020-04-17 00:51:43 +0900

    UPDATE: mir Data

--- src/data/mirData.ts
+++ src/data/mirData.ts
@@ -32,6 +32,46 @@ import artwork31 from 'assets/mir/artwork31.png';
 import artwork32 from 'assets/mir/artwork32.png';
 import artwork33 from 'assets/mir/artwork33.png';
 
+import artwork1_1 from 'assets/mir/data/1/1.png'
+import artwork1_2 from 'assets/mir/data/1/2.png'
+import artwork1_3 from 'assets/mir/data/1/3.png'
+import artwork2_1 from 'assets/mir/data/2/1.png'
+import artwork2_2 from 'assets/mir/data/2/2.gif'
+import artwork3_1 from 'assets/mir/data/3/1.jpeg'
+import artwork3_2 from 'assets/mir/data/3/2.jpg'
+import artwork4_1 from 'assets/mir/data/4/1.png'
+import artwork5_1 from 'assets/mir/data/5/1.png'
+import artwork5_2 from 'assets/mir/data/5/2.gif'
+import artwork5_3 from 'assets/mir/data/5/3.gif'
+import artwork6_1 from 'assets/mir/data/6/1.jpg'
+import artwork7_1 from 'assets/mir/data/6/1.jpg'
+import artwork8_1 from 'assets/mir/data/8/1.jpg'
+import artwork9_1 from 'assets/mir/data/9/1.jpg'
+import artwork10_1 from 'assets/mir/data/10/1.jpg'
+import artwork11_1 from 'assets/mir/data/11/1.png'
+import artwork12_1 from 'assets/mir/data/12/1.png'
+import artwork13_1 from 'assets/mir/data/13/1.png'
+import artwork14_1 from 'assets/mir/data/14/1.png'
+import artwork15_1 from 'assets/mir/data/15/1.png'
+import artwork16_1 from 'assets/mir/data/16/1.png'
+import artwork17_1 from 'assets/mir/data/17/1.png'
+import artwork18_1 from 'assets/mir/data/18/1.jpg'
+import artwork19_1 from 'assets/mir/data/19/1.jpg'
+import artwork20_1 from 'assets/mir/data/20/1.png'
+import artwork21_1 from 'assets/mir/data/21/1.png'
+import artwork22_1 from 'assets/mir/data/22/1.png'
+import artwork23_1 from 'assets/mir/data/23/1.png'
+import artwork24_1 from 'assets/mir/data/24/1.png'
+import artwork25_1 from 'assets/mir/data/25/1.png'
+import artwork26_1 from 'assets/mir/data/26/1.png'
+import artwork27_1 from 'assets/mir/data/27/1.png'
+import artwork28_1 from 'assets/mir/data/28/1.png'
+import artwork29_1 from 'assets/mir/data/29/1.png'
+import artwork30_1 from 'assets/mir/data/30/1.png'
+import artwork31_1 from 'assets/mir/data/31/1.png'
+import artwork32_1 from 'assets/mir/data/32/1.png'
+import artwork33_1 from 'assets/mir/data/33/1.png'
+
 const mirData: Array<ArtworkType> = [
   {
     thumbnail: artwork1,
@@ -40,10 +80,10 @@ const mirData: Array<ArtworkType> = [
     description: "우리들의 눈에는 보이지 않지만 가까운 곳에서 열심히 일하는 꽃들의 작은 기사들. 플로라 나라의 꽃가루 요정들은 자신이 맡은 계절을 …",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork1_1, artwork1_2, artwork1_3],
       profile: {
-        name: '',
-        email: '',
+        name: '강민지',
+        email: 'bosa112@naver.com',
         description: '우리들의 눈에는 보이지 않지만 가까운 곳에서 열심히 일하는 꽃들의 작은 기사들. 플로라 나라의 꽃가루 요정들은 자신이 맡은 계절을 아름답게 가꾸기 위해 노력합니다.'
       },
     }
@@ -54,11 +94,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "샤샤는 모두가 잠든 사이에 인간세계에 내려오는 마물들을 물리치는 마법소녀이다. 평범한 고등학생 시아는 하늘에 모두를 지키고 싶다는…",
     data: {
-      images: [],
+      images: [artwork2_1, artwork2_2],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '강민지',
+        email: 'bosa112@naver.com',
+        description: '샤샤는 모두가 잠든 사이에 인간세계에 내려오는 마물들을 물리치는 마법소녀이다.</br>평범한 고등학생 시아는 하늘에 모두를 지키고 싶다는 소원을 빌고 마법의 힘을 얻게 된다.</br>그 후 마법소녀가 되어 마물들을 물리치며 세계의 평화를 지킨다.'
       },
     }
   },
@@ -68,10 +108,10 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "물고기를 찾아 직접 바다로 들어가게 된 고양이! 호흡하기 위해서 어항을 머리에 쓰고 바닷속으로 들어간다. 고양이가 들어간 바닷속은 …",
     data: {
-      images: [],
+      images: [artwork3_1, artwork3_2],
       profile: {
-        name: '',
-        email: '',
+        name: '강민지',
+        email: 'bosa112@naver.com',
         description: '물고기를 찾아 직접 바다로 들어가게 된 고양이! 호흡하기 위해서 어항을 머리에 쓰고 바닷속으로 들어간다. 고양이가 들어간 바닷속은 다양한 물고기와 해파리, 심지어 불가사리까지 있었다. 꿈꾸던 물고기들을 마주친 고양이의 심정을 반짝거리는 눈으로 표현했다.'
       },
     }
@@ -82,11 +122,11 @@ const mirData: Array<ArtworkType> = [
     type: "POSTER",
     description: "미르를 상징하는 색인 분홍색과 연보라색을 주요색으로 사용하였다. 어떤 그림을 그려넣어야 조금 더 미르라는 것이 돋보일까 고민하다 …",
     data: {
-      images: [],
+      images: [artwork4_1],
       profile: {
-        name: '',
+        name: '김은서',
         email: '',
-        description: ''
+        description: '미르를 상징하는 색인 분홍색과 연보라색을 주요색으로 사용하였다. 어떤 그림을 그려넣어야 조금 더 미르라는 것이 돋보일까 고민하다 동잠을 입은 미르친구들을 그려넣기로 하고 그린 그림이다.'
       },
     }
   },
@@ -97,11 +137,11 @@ const mirData: Array<ArtworkType> = [
     description: "항상 서울진출을 꿈꾸는 시골텃밭 야채친구들이 스타가 되고 싶어서 이모티콘이 되기로 하고 만들어진 야채 6총사.",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork5_1, artwork5_2, artwork5_3],
       profile: {
-        name: '',
+        name: '김은서',
         email: '',
-        description: ''
+        description: '항상 서울진출을 꿈꾸는 시골텃밭 야채친구들이 스타가 되고 싶어서 이모티콘이 되기로 하고 만들어진 야채 6총사'
       },
     }
   },
@@ -111,11 +151,11 @@ const mirData: Array<ArtworkType> = [
     type: "POSTER",
     description: "미르는 “용”을 뜻하는 말로, 순우리말이다. 그래서 포스터에 용을 나타내어 미르를 표현하였고, 학생의 의상은 미르 동잠으로 표현하였다….",
     data: {
-      images: [],
+      images: [artwork6_1],
       profile: {
-        name: '',
+        name: '김자현',
         email: '',
-        description: ''
+        description: '미르는 “용”을 뜻하는 말로, 순우리말이다.</br>그래서 포스터에 용을 나타내어 미르를 표현하였고, 학생의 의상은 미르 동잠으로 표현하였다.'
       },
     }
   },
@@ -125,11 +165,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "파란 하늘과 학생을 그려서 밝은 느낌을 주었다. 약간 부스스한 헤어스타일과 손동작, 옆을 바라보는 시선, 마지막으로 코에 붙인 밴드를 …",
     data: {
-      images: [],
+      images: [artwork7_1],
       profile: {
-        name: '',
+        name: '김자현',
         email: '',
-        description: ''
+        description: '파란 하늘과 학생을 그려서 밝은 느낌을 주었다. 약간 부스스한 헤어스타일과 손동작, 옆을 바라보는 시선, 마지막으로 코에 붙인 밴드를 그려 장난기가 많은 학생을 표현하였다.'
       },
     }
   },
@@ -139,11 +179,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "동양풍 그림으로 한복을 그렸고 머리도 길게 그려 표현하였다. 칼을 잘 쓰는 무사를 그렸다.",
     data: {
-      images: [],
+      images: [artwork8_1],
       profile: {
-        name: '',
+        name: '김자현',
         email: '',
-        description: ''
+        description: '동양풍 그림으로 한복을 그렸고 머리도 길게 그려 표현하였다. 칼을 잘 쓰는 무사를 그렸다.'
       },
     }
   },
@@ -154,11 +194,11 @@ const mirData: Array<ArtworkType> = [
     description: "학교축제 테이트 부서에서 스티커로 내려고 만든 작품입니다. 도트로 최대한 할로윈 분위기를 살려 만든 작품입니다.",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork9_1],
       profile: {
-        name: '',
+        name: '김자현',
         email: '',
-        description: ''
+        description: '은발 여자의 옆모습을 그렸고 꽃을 추가하여 그림이 조금 더 화사하게 보일 수 있도록 하였다. 배경이 없어 심심해 보일 수 있어서 부가적으로 뒤에 무늬를 넣어 여백을 채워줬다.'
       },
     }
   },
@@ -168,11 +208,11 @@ const mirData: Array<ArtworkType> = [
     type: "CHARACTER DESIGN",
     description: "캐릭터 디자인 수행평가를 위해 만든 작품입니다.",
     data: {
-      images: [],
+      images: [artwork10_1],
       profile: {
-        name: '',
+        name: '김자현',
         email: '',
-        description: ''
+        description: '귀 모양을 특이하게 표현하여 사람을 그린 그림이다. 중간에 밝게 표현한 부분은 문 사이로 들어오는 빛을 표현한 것이다. 사람이 안고 있는 생명체도 특이하게 그렸다.'
       },
     }
   },
@@ -182,11 +222,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "메이플스토리의 호영 캐릭터 특유의 장난스러움과 당돌함을 표현하고자했다. 손에 든 접선(摺扇)과 매듭, 그리고 허리춤에 당당히 자리하는…",
     data: {
-      images: [],
+      images: [artwork11_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '메이플스토리의 호영 캐릭터 특유의 장난스러움과 당돌함을 표현하고자했다. 손에 든 접선(摺扇)과 매듭, 그리고 허리춤에 당당히 자리하는 마패를 신경쓰며 그렸다.'
       },
     }
   },
@@ -196,11 +236,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "버스킹으로 기타와 드럼을 연주하는 것처럼 관현악을 연주하는 건 어떨까? 란 생각을 가지고 그렸다. 어두운 채도에 눈에 띄는 색을 포인트…",
     data: {
-      images: [],
+      images: [artwork12_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '버스킹으로 기타와 드럼을 연주하는 것처럼 관현악을 연주하는 건 어떨까? 란 생각을 가지고 그렸다. 어두운 채도에 눈에 띄는 색을 포인트로 넣어 강조했다. 또 첼로와 바이올린, 셔츠에 해골을 삽입해 펑키한 느낌을 표현하고자했다. '
       },
     }
   },
@@ -211,11 +251,11 @@ const mirData: Array<ArtworkType> = [
     description: "옛날 교과서에 있는 그림을 보며 영감을 얻었다. 하이라이트가 돋보이는 머리칼에 전체적으로 채도가 높은 색으로 채색했다. 버건디의 상의에…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork13_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '옛날 교과서에 있는 그림을 보며 영감을 얻었다. 하이라이트가 돋보이는 머리칼에 전체적으로 채도가 높은 색으로 채색했다. 버건디의 상의에 노란 리본, 한쪽 흘러내린 양말로 포인트를 주었다. '
       },
     }
   },
@@ -225,11 +265,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "파란 머리칼에 체리같은 머리끈으로 포인트를 주었다. 연하늘의 드레스가 살짝 흩날리며 들린 모습을 강조했다. 전체적으로 따듯한 색감을…",
     data: {
-      images: [],
+      images: [artwork14_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '파란 머리칼에 체리같은 머리끈으로 포인트를 주었다. 연하늘의 드레스가 살짝 흩날리며 들린 모습을 강조했다. 전체적으로 따듯한 색감을 주어 활기찬 느낌을 표현했다.'
       },
     }
   },
@@ -239,11 +279,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "어린 아이가 몸을 구부리고 자는 모습을 그렸다. 품에는 미르를 상징하는 분홍 용 인형을 안겨주었다. 천체적으로 따듯한 색감을 써 밝은 …",
     data: {
-      images: [],
+      images: [artwork15_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '어린 아이가 몸을 구부리고 자는 모습을 그렸다. 품에는 미르를 상징하는 분홍 용 인형을 안겨주었다. 천체적으로 따듯한 색감을 써 밝은 분위기를 그렸다. 하단에는 조그마한 강아지를 그려 귀여움을 더했다.'
       },
     }
   },
@@ -253,11 +293,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "채도가 높고 대조를 이루는 색을 사용해 화려함을 표현했다. 두 소녀가 입고있는 외투는 미르의 야구자켓이며, 기하학적인 도형을 배경으로…",
     data: {
-      images: [],
+      images: [artwork16_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '채도가 높고 대조를 이루는 색을 사용해 화려함을 표현했다. 두 소녀가 입고있는 외투는 미르의 야구자켓이며, 기하학적인 도형을 배경으로 두어 심심하지 않게 했다.'
       },
     }
   },
@@ -268,11 +308,11 @@ const mirData: Array<ArtworkType> = [
     description: "신비로운 느낌을 주기 위해 보라 계열을 사용하여 작업한 딸기 케이크 픽셀아트입니다. ",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork17_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김혜진',
+        email: 'jinjins0629@gmail.com',
+        description: '게임 걸즈밴드파티의 팬아트이다. 각 캐릭터가 위에서 아래로 떨어지는 모습을 그렸다. 캐릭터가 가지고있는 각각의 성격과 특징을 그려냈고 전체적으로 귀여운 분위기를 자아낸다.'
       },
     }
   },
@@ -282,11 +322,11 @@ const mirData: Array<ArtworkType> = [
     type: "POSTER",
     description: "여러 패턴을 디자인해서 한 페이지에 모았다. 패턴/그림을 하나씩 뜯어서 보면 디테일함을 볼 수 있다….",
     data: {
-      images: [],
+      images: [artwork18_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '박지형',
+        email: 'sidsidsid328@naver.com',
+        description: '여러 패턴을 디자인해서 한 페이지에 모았다. 패턴/그림을 하나씩 뜯어서 보면 디테일함을 볼 수 있다.'
       },
     }
   },
@@ -296,11 +336,11 @@ const mirData: Array<ArtworkType> = [
     type: "EDITORIAL DESIGN",
     description: "새파란 물 속에 가라앉고 싶을때의 기분을 표현했다. 들어가보면 생각보다 시원하지 않을까?",
     data: {
-      images: [],
+      images: [artwork19_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '박지형',
+        email: 'sidsidsid328@naver.com',
+        description: '새파란 물 속에 가라앉고 싶을때의 기분을 표현했다. 들어가보면 생각보다 시원하지 않을까?'
       },
     }
   },
@@ -310,11 +350,11 @@ const mirData: Array<ArtworkType> = [
     type: "POSTER",
     description: "미르를 홍보하는 포스터이다.",
     data: {
-      images: [],
+      images: [artwork20_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '염지원',
+        email: 'yjo3834@naver.com',
+        description: '미르를 홍보하는 포스터이다'
       },
     }
   },
@@ -325,11 +365,11 @@ const mirData: Array<ArtworkType> = [
     description: "클립스튜디오를 사용한 동양풍 일러스트이다",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork21_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '염지원',
+        email: 'yjo3834@naver.com',
+        description: '클립스튜디오를 사용한 동양풍 일러스트이다'
       },
     }          
   },
@@ -339,11 +379,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "로젤리아의 리더를 표현한 일러스트",
     data: {
-      images: [],
+      images: [artwork22_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '염지원',
+        email: 'yjo3834@naver.com',
+        description: '로젤리아의 리더를 표현한 일러스트'
       },
     }
   },
@@ -353,11 +393,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "포켓몬스터소드의 물타입 체육관 관장과 스타팅포켓몬이다.",
     data: {
-      images: [],
+      images: [artwork23_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '염지원',
+        email: 'yjo3834@naver.com',
+        description: '포켓몬스터소드의 물타입 체육관 관장과 스타팅포켓몬이다.'
       },
     }
   },
@@ -367,11 +407,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "디자인한 캐릭터의 설정을 살리기위해 제작한 장면 중 하나이다. 요리 연습을 위해 만든 스파게티를 처음 맛봤지만 맛이 없어서 충격받은…",
     data: {
-      images: [],
+      images: [artwork24_1],
       profile: {
-        name: '',
+        name: '장하민',
         email: '',
-        description: ''
+        description: '디자인한 캐릭터의 설정을 살리기위해 제작한 장면 중 하나이다.</br>요리 연습을 위해 만든 스파게티를 처음 맛봤지만 맛이 없어서 충격받은 상황이다. </br>상황을 살리기 위해 채도 낮은 보라색과 초록색(식욕감퇴를 부르는 색들)을 사용하였다.'
       },
     }
   },
@@ -382,11 +422,11 @@ const mirData: Array<ArtworkType> = [
     description: "무더운 여름날, 평상에 앉아 아이스크림으로 더위를 달래는 아이를 표현하였다. 비교적 단조로워 보이는 상단부분을 채우기 위해 SUMMER…",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork25_1],
       profile: {
-        name: '',
+        name: '장하민',
         email: '',
-        description: ''
+        description: '무더운 여름날, 평상에 앉아 아이스크림으로 더위를 달래는 아이를 표현하였다. 비교적 단조로워 보이는 상단부분을 채우기 위해 SUMMER이라는 문구를 써넣었다.'
       },
     }
   },
@@ -396,11 +436,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "초롱 아귀와 물고기를 소재로 그린 장면이다. 보석에 눈이 팔려 아귀의 입을 보지 못한 물고기가 잡아먹히기 직전의 모습을 담았다.",
     data: {
-      images: [],
+      images: [artwork26_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '장하민',
+        email: '', 
+        description: '초롱 아귀와 물고기를 소재로 그린 장면이다.</br>보석에 눈이 팔려 아귀의 입을 보지 못한 물고기가 잡아먹히기 직전의 모습을 담았다.</br>*보석은 초롱 아귀의 머리에 달린 발광체를 표현한 것이다.'
       },
     }
   },
@@ -410,11 +450,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "동아리 홍보를 목적으로 그린 그림이다. 캐릭터가 그림을 보고 있는 관람자에게 손을 내미는 동작으로 초대와 환영을 표현하였다….",
     data: {
-      images: [],
+      images: [artwork27_1],
       profile: {
-        name: '',
+        name: '장하민',
         email: '',
-        description: ''
+        description: '동아리 홍보를 목적으로 그린 그림이다.</br>캐릭터가 그림을 보고 있는 관람자에게 손을 내미는 동작으로 초대와 환영을 표현하였다.</br>미르를 의미하는 애니실(미르 부실), 그림 그리던 종이, 미르 동아리 잠바를 입은 캐릭터를 그림에 넣었다.'
       },
     }
   },
@@ -424,11 +464,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "엄마는 같지만 아빠가 다른 남매다. 프랑켄슈타인+페어리 혼혈인 대학생 고교마와 인간+페어리 혼혈인 고등학생 주담. 담은 교마를 별로 안…",
     data: {
-      images: [],
+      images: [artwork28_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: '엄마는 같지만 아빠가 다른 남매다.프랑켄슈타인+페어리 혼혈인 대학생 고교마와 인간+페어리 혼혈인 고등학생 주담.</br>담은 교마를 별로 안 좋아하지만 교마는 끊임없이 담에게 다가간다.'
       },
     }
   },
@@ -439,11 +479,11 @@ const mirData: Array<ArtworkType> = [
     description: "밤바다를 항해하고 있는 배가 꽃잎들을 바다에 뿌리고 있다. '나'를 떠나가는 사람들을 붙잡고 싶다고 그 자리에 멈춰서 기다리고 있을 수는 …",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork29_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: "밤바다를 항해하고 있는 배가 꽃잎들을 바다에 뿌리고 있다.</br>'나'를 떠나가는 사람들을 붙잡고 싶다고 그 자리에 멈춰서 기다리고 있을 수는 없다.</br>'그'에게도 길이 있는 것처럼 '나'도 '나'의 길을 나아가야한다.</br>그래서 그냥 나아가기로 한다.</br>그치만 언제든지 다시 돌아오라고 꽃잎을 바다에 뿌린다.</br>'그'가 보고 찾아올 수 있도록."
       },
     }
   },
@@ -453,11 +493,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "해파리와 고양이가 동료가 됐다. 해파리와 함께 바다로 들어간 고양이는 다른 해파리들과 인사를 나누고 물고기의 행방을 묻는다….",
     data: {
-      images: [],
+      images: [artwork30_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: '해파리와 고양이가 동료가 됐다.</br>해파리와 함께 바다로 들어간 고양이는 다른 해파리들과 인사를 나누고 물고기의 행방을 묻는다.</br>어쩌면 고양이는 그저 직접 물고기를 사냥해 먹고 싶었을 뿐일지도 모른다.'
       },
     }
   },
@@ -467,11 +507,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "19기 미르 부원들을 전부 그렸다. 코로나만 없었으면 이 포스터는 이미 페이스북에서 동아리 홍보를 위해 쓰이고 있었을 것이다….",
     data: {
-      images: [],
+      images: [artwork31_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: '19기 미르 부원들을 전부 그렸다.</br>코로나만 없었으면 이 포스터는 이미 페이스북에서 동아리 홍보를 위해 쓰이고 있었을 것이다.'
       },
     }
   },
@@ -481,11 +521,11 @@ const mirData: Array<ArtworkType> = [
     type: "ILLUSTRATION",
     description: "원래는 그냥 숏컷인 소녀가 비니를 쓰고 있기만 한 그림이었다. 막상 그리다보니 목이 너무 길어서 목에 링들을 끼워서 목의 길이를 늘리는…",
     data: {
-      images: [],
+      images: [artwork32_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: '원래는 그냥 숏컷인 소녀가 비니를 쓰고 있기만 한 그림이었다.</br>막상 그리다보니 목이 너무 길어서 목에 링들을 끼워서 목의 길이를 늘리는 아프리카 부족을 생각하면서 바뀌었다.'
       },
     }
   },
@@ -496,11 +536,11 @@ const mirData: Array<ArtworkType> = [
     description: "컴퓨터그래픽시간에 캐릭터디자인 수행평가를 위해 만든 게임 캐릭터다. '수'라는 이름은 영단어 'sew(꿰매다)'에서 가져왔다….",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork33_1],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최가은',
+        email: 'angle321@naver.com',
+        description: "컴퓨터그래픽시간에 캐릭터디자인 수행평가를 위해 만든 게임 캐릭터다.</br>'수'라는 이름은 영단어 'sew(꿰매다)'에서 가져왔다.</br>'수'가 흩어진 자신의 신체 부위들을 찾아서 꿰매며 이야기가 진행된다.</br>캐릭터를 만들고 스토리를 짜는 과정에서 머릿속에서는 이미 하나의 게임이 완성됐다."
       },
     }
   },
