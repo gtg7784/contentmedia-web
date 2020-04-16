54b42299d62d657dce14ed0e638bbcac8653e0db
gtg7784 <gtg7784@naver.com>
2020-04-17 00:51:33 +0900

    UPDATE: junr Data

--- src/data/junrData.ts
+++ src/data/junrData.ts
@@ -25,9 +25,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '자의누리',
         email: '',
-        description: ''
+        description: '자의누리 11기 학생들이 진행한 키네틱 타이포그래피입니다.'
       },
       link: "https://www.youtube.com/watch?v=U8HVOlVVW-4"
     }
@@ -40,9 +40,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '2019년도 그래픽 공모전 모션그래픽 분야 동상 수상작입니다. 일러스트로 만든 캐릭터와 배경 등을 애프터 이펙트로 모션을 주어서 만든 인포그래픽으로, 게임이라는 하나의 문화가 권위 있는 집단에 의하여 잘못된 방향으로 질병 화가 되는 것에 대해 더욱 많은 사람들이 이러한 상황이 있다는 걸 알고 문제의 심각성을 인지하기를 바라는 목적으로 제작한 영상입니다. 시청자에 빠르고 쉬운 이해를 위해 최대한 심플하고 쳐지지않도록 장면들을 구성하여 제작했습니다.'
       },
       link: "https://www.youtube.com/watch?v=Ag8ds27l0SE"
     }
@@ -55,9 +55,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '슬라이드 디자인 영상입니다. 각 슬라이드들이 주로 쇼릴 형식으로 넘어갑니다. 이렇기에 장면 장면이 넘어가는 트렌지션이 영상 흐름에 가장 큰 영향을 줄것이라고 생각하여 트렌지션을 독특하고 다양한 모션으로 구상하여 영상이 지루해 지지않도록 만들었습니다. '
       },
       link: "https://www.youtube.com/watch?v=j4AJh_AWECQ"
     }
@@ -70,9 +70,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: ' 인트로 모션그래픽 영상입니다. 임팩트있고 트렌디한 분위기로 제작한 작품입니다. 이 영상은 굉장히 짧은시간으로 구성되어 있어 정보전달 없이 너무 빠르게 지나가 버릴 수 있다고 생각했습니다. 그래서 최대한 적은 텍스트와, 적지만 영상이 지루해지지 않을정도의 모션을 넣어 영상을 완성하였습니다.'
       },
       link: "https://www.youtube.com/watch?v=9dsgnGlcjJE"
     }
@@ -86,9 +86,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '저만의 유튜브를 만든다면 쓰게 될 인트로를 예상하여 만들어본 작품입니다. 애프터 이펙트로 제작한 영상으로 다른 사람들이 후회되는 구매를 하지 않도록 먼저 사고 리뷰를 해준다는 컨셉을 잡았습니다. 그리고 이 컨셉에 맞는 텍스트 들과 리드미컬한 노래에 박자에 맡게 모션을 넣어줌으로써 영상이 지루해지거나 루즈해지지 않도록 하였습니다. 또한 영상 앞에 부분에서는 이 채널에 대한 설명을 진행하고 후반 부에는 대표색과 로고를 배치시켜 짧은 시간 안에 이 채널에 대한 정보를 전달할 수 있도록 만들었습니다.'
       },
       link: "https://www.youtube.com/watch?v=5VnAhI9uBA0"
     }
@@ -101,9 +101,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '애프터 이펙트 연습을 위해 제작한 영상입니다. 기존과는 다른 느낌에 영상을 제작 해보려고 모작을 해보았습니다. 원작품은 영업비밀 디자이너님의 작품입니다. 지금까지 해보지 못한 스타일의 작업을 해보니 더욱더 많은 시각을 가질 수 있었던 경험이였습니다.'
       },
       link: "https://www.youtube.com/watch?v=hX8KR2mxH_k"
     }
@@ -116,9 +116,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '에어팟 프로 광고를 모작한 영상입니다. 기존에 사용하지 않던 이펙트들을 많이 다루어 볼 수 있는 좋은 경험 이였습니다.'
       },
     }
   },
@@ -130,9 +130,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
+        name: '남민우',
         email: '',
-        description: ''
+        description: '이번 시연회에서 자의누리를 소개 할 목적으로 제작한  자의누리 셀프브랜딩 영상입니다. 리드미컬하고 트렌디한 노래와 그에 맞는 모션으로 영상을 구성하였으며 전체적으로 자의누리의 대표색인 파란색을 사용하여 영상 전체의 통일감을 주었습니다. 그리고 영상 이 짧기 떄문에 그 짧은시간안에 최대한 효율적으로 자의누리를 소개하는데 초점을 두고 만들었습니다.'
       },
       link: "https://www.youtube.com/watch?v=jeYWXOQ7Sac"
     }
@@ -146,9 +146,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '문은빈',
+        email: 'skybin0627@naver.com',
+        description: '영상공모전 영화 부문 금상 수상작으로 청소년들의 도박 문제가 예전보다 심각해지고 있습니다. 제목 이름 시나브로 뜻 처럼 모르는 사이에 조금씩 조금씩 도박을 즐기고 있는 청소년들이 하루빨리 도박의 심각성을 깨달았으면 하는 마음에서 만들었습니다.'
       },
       link: "https://www.youtube.com/watch?v=GHyQIkHRwxI"
     }
@@ -161,9 +161,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '이주연',
+        email: 'jjuu1621@naver.com',
+        description: '영상공모전 은상을 수상작입니다. 이 광고로 자주 애용하는 어플리케이션인 카카오맵을 광고하는 동시에, 특별하고 깐깐하게 선정된 테마지도 기능을 중점적으로 설명하여 사람들에게 이 기능을 더 널리 알리기위해 제작하였다. 밝고 긍정적인 분위기를 바탕으로 전체적으로 카카오맵만의 색깔을 살리기 위해 생동감 있는 느낌을 내며 진행하였다. 또한 스토리로는 누구나 느꼈을법한 지루한 일상에서 시작하여 카카오맵이 이끄는데로 좋은 장소들을 구경하며 떠나보는 스토리로 더욱 친숙한 느낌을 내고자 하였다.'
       },
       link: "https://www.youtube.com/watch?v=4Jjr7upVieg"
     }
@@ -176,9 +176,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '이주연',
+        email: 'jjuu1621@naver.com',
+        description: '나만의 인트로를 만들기위해 나의 머릿속안에 자리잡고있는 내가 좋아하는 것들을 이용하여 만들었습니다. 내 머릿속 생각들중 상징적인것들을 위주로 추가하였고, 광활한 우주처럼 펼쳐진 내 생각을 배경으로 제작하였습니다. 전체적으로 움직이는 듯한 느낌을 내기위해 모션을 추가하였고 네온사인효과를 이용하여 직접 켜지는 모션을 통해 생동감을 표현했습니다.'
       },
       link: "https://www.youtube.com/watch?v=gtCd0sj-xZI"
     }
@@ -191,9 +191,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김휘재',
+        email: 'dusan960766@gmail.com',
+        description: 'FICTIST의 Lilzyz님이 주최하신 대규모 합작에 참여하여 HAON 파트를 담당해 제작한 타이포그래피입니다.  저에게는 2019년의 시작을 알리는 작품이었고, 이런 대규모 프로젝트에 참여하게 되어 좋은 경험이었습니다.'
       },
       link: "https://www.youtube.com/watch?v=NVjtdzAfDUY",
     }
@@ -207,9 +207,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '김희서',
+        email: 'hitit0727@naver.com',
+        description: '화석 연료와 신재생 에너지 - 그래픽공모전 금상 수상작입니다. 환경 오염이 심해지는 지구, 모든 생물들이 살아가는 자연을 인간이 스스로 무너뜨리고 있습니다. 화석연료를 통해 환경 파괴를 야기하는 원인을 알아보고 신재생 에너지, 특히 수소 연료 전지가 어떻게 이를 막을 수 있는 지에 대해 소개합니다.'
       },
       link: "https://www.youtube.com/watch?v=ygICQn2knSE"
     }
@@ -222,9 +222,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '이충혁',
+        email: 'leechh0948@naver.com',
+        description: '2020년도에 고등학교로 올라가는 중학교 3학년 학생들이 진학에 대해서 고민하고 있을 시기에 콘텐츠 디자인과를 홍보하는 목적으로 만든 영상입니다. 지금까지 멀티미디어과 (콘텐츠 디자인과) 학생들이 만든 작품들을 모아서 쇼릴 형식으로 만들어 더욱 의미있는 작품입니다. 이 영상은 쇼릴 형식이기 때문에 중후반 부분에서는 조금 루즈(지루) 해질 수 있다고 생각했습니다. 만들어져있는 작품들을 기승전결이 없이 그대로 컷편집만 했기 때문이죠. 그래서 초반 인트로에서 이목을 사로잡아야겠다는 마음으로 사운드에 맞춘 화려한 모션그래픽을 넣어 완성하였습니다.'
       },
       link: "https://www.youtube.com/watch?v=GfnSh4gzaBw"
     }
@@ -237,9 +237,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '이충혁',
+        email: 'leechh0948@naver.com',
+        description: "2019년도 선린인터넷고등학교 그래픽 공모전 모션그래픽 부문에서 대상(1위)을 탄 작품입니다. 일러스트로 만든 캐릭터와 배경 등을 에프터 이펙트로 모션을 주어서 만든 인포그래픽으로, 번아웃 증후군이 무엇인지, 왜 발생하는지, 해결책과 예방법 등을 구체적으로 담아 만들었습니다. 인포그래픽에서 가장 많이 고민해야할 것은 바로 '전달력'입니다. 인포그래픽은 이름 그대로 정보를 전달하는 것을 목적으로 하기 때문에 영상이 아무리 멋지고, 예뻐도 정보 전달이 제대로 이루어지지 않는다면 그 영상은 실패한 영상이라고 할 수 있습니다. 그래서 제작할 때에 대본에 맞춰 일러스트 작업과 모션그래픽 작업을 할 때, 심플하면서 가장 이해하기 쉽게 장면들을 구성하며 제작하였습니다."
       },
       link: "https://www.youtube.com/watch?v=kaAVJgJvFa8"
     }
@@ -252,9 +252,9 @@ const junrData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '',
-        email: '',
-        description: ''
+        name: '최우석, 지제준, 황보태현, 허준호',
+        email: 'jabez0106@naver.com',
+        description: ' 영상공모전 영화 부문 대상 수상작입니다.'
       },
       link: "https://www.youtube.com/watch?v=V8r4OIdk9TY"
     }
