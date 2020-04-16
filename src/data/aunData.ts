854f95eefdffcf3b2b24c547e97bd846646c1f9b
gtg7784 <gtg7784@naver.com>
2020-04-17 00:51:19 +0900

    UPDATE: aun Data

--- src/data/aunData.ts
+++ src/data/aunData.ts
@@ -1,6 +1,37 @@
 import artwork1 from 'assets/aun/artwork1.png';
 import artwork2 from 'assets/aun/artwork2.png';
 
+import artwork1_1 from 'assets/aun/data/1/1.jpg';
+import artwork1_2 from 'assets/aun/data/1/2.jpg';
+import artwork1_3 from 'assets/aun/data/1/3.jpg';
+import artwork1_4 from 'assets/aun/data/1/4.jpg';
+import artwork1_5 from 'assets/aun/data/1/5.jpg';
+import artwork1_6 from 'assets/aun/data/1/6.jpg';
+import artwork1_7 from 'assets/aun/data/1/7.jpg';
+import artwork1_8 from 'assets/aun/data/1/8.jpg';
+import artwork1_9 from 'assets/aun/data/1/9.jpg';
+import artwork1_10 from 'assets/aun/data/1/10.jpg';
+import artwork1_11 from 'assets/aun/data/1/11.jpg';
+import artwork1_12 from 'assets/aun/data/1/12.jpg';
+import artwork1_13 from 'assets/aun/data/1/13.jpg';
+import artwork1_14 from 'assets/aun/data/1/14.jpg';
+import artwork1_15 from 'assets/aun/data/1/15.jpg';
+import artwork1_16 from 'assets/aun/data/1/16.jpg';
+import artwork1_17 from 'assets/aun/data/1/17.jpg';
+
+import artwork2_1 from 'assets/aun/data/2/1.jpg';
+import artwork2_2 from 'assets/aun/data/2/2.jpg';
+import artwork2_3 from 'assets/aun/data/2/3.jpg';
+import artwork2_4 from 'assets/aun/data/2/4.jpg';
+import artwork2_5 from 'assets/aun/data/2/5.jpg';
+import artwork2_6 from 'assets/aun/data/2/6.jpg';
+import artwork2_7 from 'assets/aun/data/2/7.jpg';
+import artwork2_8 from 'assets/aun/data/2/8.jpg';
+import artwork2_9 from 'assets/aun/data/2/9.jpg';
+import artwork2_10 from 'assets/aun/data/2/10.jpg';
+import artwork2_11 from 'assets/aun/data/2/11.jpg';
+import artwork2_12 from 'assets/aun/data/2/12.jpg';
+
 const aunData: Array<ArtworkType> = [
   {
     thumbnail: artwork1,
@@ -9,10 +40,10 @@ const aunData: Array<ArtworkType> = [
     description: "2019년 여름방학 때 22기, 23기 일부 부원들 및 지인들과 함께 촬영한 작품입니다. 동정심에 처음 보는 사람을 위해 노력한 것이 자신이 …",
     style: { marginLeft: 0 },
     data: {
-      images: [],
+      images: [artwork1_1, artwork1_2, artwork1_3, artwork1_4, artwork1_5, artwork1_6, artwork1_7, artwork1_8, artwork1_9, artwork1_10, artwork1_11, artwork1_12, artwork1_13, artwork1_14, artwork1_15, artwork1_16, artwork1_17],
       profile: {
-        name: '아우내',
-        email: '',
+        name: '최정규, 정기재',
+        email: 'jjascd@naver.com, kijae2011@gmail.com',
         description: '"Puzzle"은 아우내 24기 신입 부원 모집을 위해 제작된 홍보영상입니다. 아우내에서 영화를 찍는 것처럼, 작품에는 영화를 찍는 과정을 각색하여 표현하였습니다.</br>작품의 제목이 "Puzzle"인 이유는 처음에는 감독을 맡은 친구 혼자만 있었으나 영화 촬영에 필요한 스태프들이 하나둘씩 모이면서 하나의 팀을 이루게 되고, 그 들이 다 같이 영화를 만드는 과정이 마치 퍼즐을 한 조각씩 맞춰 작품이 완성된다는 점이 유사하다고 생각하여 제목을 이렇게 정했습니다.</br>이번 작품은 2020년, 해가 바뀌고 나서 23기 부원들끼리 만든 첫 작품이라는 점에서 의미있는 작품이라고 생각합니다.'
       }
     }
@@ -25,8 +56,8 @@ const aunData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '아우내',
-        email: '',
+        name: '정기재, 손성민',
+        email: 'kijae2011@gmail.com, sunyoung9173@gmail.com',
         description: '작품설명 및 후기: "Puzzle"은 아우내 24기 신입 부원 모집을 위해 제작된 홍보영상입니다. 아우내에서 영화를 찍는 것처럼, 작품에는 영화를 찍는 과정을 각색하여 표현하였습니다.</br>작품의 제목이 "Puzzle"인 이유는 처음에는 감독을 맡은 친구 혼자만 있었으나 영화 촬영에 필요한 스태프들이 하나둘씩 모이면서 하나의 팀을 이루게 되고, 그 들이 다 같이 영화를 만드는 과정이 마치 퍼즐을 한 조각씩 맞춰 작품이 완성된다는 점이 유사하다고 생각하여 제목을 이렇게 정했습니다.</br>이번 작품은 2020년, 해가 바뀌고 나서 23기 부원들끼리 만든 첫 작품이라는 점에서 의미있는 작품이라고 생각합니다.'
       },
       link: "https://www.youtube.com/watch?v=G4F6vlqpeAI"
@@ -38,10 +69,10 @@ const aunData: Array<ArtworkType> = [
     type: "MOVIE MAKING",
     description: '"Puzzle"은 아우내 24기 신입 부원 모집을 위해 제작된 홍보영상입니다. 아우내에서 영화를 찍는 것처럼, 작품에는 영화를 찍는 과정을 …',
     data: {
-      images: [],
+      images: [artwork2_1, artwork2_2, artwork2_3, artwork2_4, artwork2_5, artwork2_6, artwork2_7, artwork2_8, artwork2_9, artwork2_10, artwork2_11, artwork2_12],
       profile: {
-        name: '아우내',
-        email: '',
+        name: '정기재, 손성민',
+        email: 'kijae2011@gmail.com, sunyoung9173@gmail.com',
         description: '작품설명 및 후기: "Puzzle"은 아우내 24기 신입 부원 모집을 위해 제작된 홍보영상입니다. 아우내에서 영화를 찍는 것처럼, 작품에는 영화를 찍는 과정을 각색하여 표현하였습니다.</br>작품의 제목이 "Puzzle"인 이유는 처음에는 감독을 맡은 친구 혼자만 있었으나 영화 촬영에 필요한 스태프들이 하나둘씩 모이면서 하나의 팀을 이루게 되고, 그 들이 다 같이 영화를 만드는 과정이 마치 퍼즐을 한 조각씩 맞춰 작품이 완성된다는 점이 유사하다고 생각하여 제목을 이렇게 정했습니다.</br>이번 작품은 2020년, 해가 바뀌고 나서 23기 부원들끼리 만든 첫 작품이라는 점에서 의미있는 작품이라고 생각합니다.'
       },
       link: "https://www.youtube.com/watch?v=cM6LKF3OhVY"
