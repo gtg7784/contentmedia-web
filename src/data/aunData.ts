3df07318afb0b41304ee27e05430ebc0f8db7247
gtg7784 <gtg7784@naver.com>
2020-04-19 20:23:39 +0900

    UPDATE: aun data

--- src/data/aunData.ts
+++ src/data/aunData.ts
@@ -56,8 +56,8 @@ const aunData: Array<ArtworkType> = [
     data: {
       images: [],
       profile: {
-        name: '정기재, 손성민',
-        email: 'kijae2011@gmail.com, sunyoung9173@gmail.com',
+        name: '아우내 전체 참여',
+        email: '',
         description: '작품설명 및 후기: "Puzzle"은 아우내 24기 신입 부원 모집을 위해 제작된 홍보영상입니다. 아우내에서 영화를 찍는 것처럼, 작품에는 영화를 찍는 과정을 각색하여 표현하였습니다.</br>작품의 제목이 "Puzzle"인 이유는 처음에는 감독을 맡은 친구 혼자만 있었으나 영화 촬영에 필요한 스태프들이 하나둘씩 모이면서 하나의 팀을 이루게 되고, 그 들이 다 같이 영화를 만드는 과정이 마치 퍼즐을 한 조각씩 맞춰 작품이 완성된다는 점이 유사하다고 생각하여 제목을 이렇게 정했습니다.</br>이번 작품은 2020년, 해가 바뀌고 나서 23기 부원들끼리 만든 첫 작품이라는 점에서 의미있는 작품이라고 생각합니다.'
       },
       link: "https://www.youtube.com/watch?v=G4F6vlqpeAI"
