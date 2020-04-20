262a48a069afaefea797b0833158f54de680fe0d
gtg7784 <gtg7784@naver.com>
2020-04-20 17:05:59 +0900

    UPDATE: eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: true,
+      es6: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
+    extends: [
+      "plugin:react/recommended",
+      "airbnb"
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly"
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+        tsx: true
+      },
+      ecmaVersion: 2018,
+      sourceType: "module"
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    plugins: [
+      "react",
+      "@typescript-eslint"
     ],
-    "rules": {
+    settings: {
+      "import/resolver": {
+        "node": {
+          "paths": ["src"],
+          "extensions": [".js", ".jsx", ".ts", ".tsx"]
+        }
+      },
+      
+    },
+    rules: {
+      "react/jsx-filename-extension": [1, {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }],
+      'react/jsx-props-no-spreading': ['off']
     }
-};
+  };
